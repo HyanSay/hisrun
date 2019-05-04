3f26194df649023bee24759dce7fcc812c8fe5c5
HyanSay <1393173280@qq.com>
2019-05-04 22:19:47 +0800

    hisrun-gateway：exercise

---
+++ hisrun-gateway/src/main/java/gateway/filter/factory/AbstractChangeRequestUriGatewayFilterFactory.java
@@ -0,0 +1,12 @@
+package gateway.filter.factory;
+
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+
+/**
+ * Email miles02@163.com
+ *
+ * @author fangzhipeng
+ * create 2018-12-04
+ **/
+public abstract class AbstractChangeRequestUriGatewayFilterFactory extends AbstractGatewayFilterFactory {
+}
